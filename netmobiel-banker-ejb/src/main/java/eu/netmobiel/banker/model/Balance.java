f8f4314f117267c4d945e77205638ddceca75f33
Jaap Reitsma <jaap.reitsma@vidinexus.com>
2020-04-07 17:46:26 +0200

    Added ledger tests.

--- netmobiel-banker-ejb/src/main/java/eu/netmobiel/banker/model/Balance.java
+++ netmobiel-banker-ejb/src/main/java/eu/netmobiel/banker/model/Balance.java
@@ -13,8 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 
