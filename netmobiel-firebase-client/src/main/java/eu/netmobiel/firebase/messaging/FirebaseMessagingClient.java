ccec14f7bd63c25fbe77713450ed9a30a922b8ae
Jaap Reitsma <jaap.reitsma@vidinexus.com>
2020-04-15 21:13:18 +0200

    Initial version of firebase messaging client.

---
+++ netmobiel-firebase-client/src/main/java/eu/netmobiel/firebase/messaging/FirebaseMessagingClient.java
@@ -0,0 +1,223 @@
+package eu.netmobiel.firebase.messaging;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.BatchResponse;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
+
+import eu.netmobiel.commons.exception.SystemException;
+import eu.netmobiel.commons.model.NetMobielMessage;
+
+/**
+ * Client for Google Firebase messaging.
+ * 
+ * @author Jaap Reitsma
+ *
+ */
+@ApplicationScoped
+public class FirebaseMessagingClient {
+    @Inject
+    private Logger log;
+
+    /**
+     * The path to the service account file for Firebird.
+     */
+    @Resource(lookup = "java:global/firebase/credentialsPath")
+    private String firebaseCredentialsPath;
+
+    /**
+     * The app 
+     */
+    private FirebaseApp app;
+    /**
+     * The messaging client
+     */
+    private FirebaseMessaging client;
+
+    /**
+     * Initializes the Firebase Messaging client. 
+     */
+    @PostConstruct
+    void initialize() {
+    	try (final InputStream is = Files.newInputStream(Paths.get(firebaseCredentialsPath))) {
+        	FirebaseOptions options = new FirebaseOptions.Builder()
+        		    .setCredentials(GoogleCredentials.fromStream(is))
+        		    .setDatabaseUrl("https://netmobiel-push.firebaseio.com/")
+        		    .build();
+
+        	app = FirebaseApp.initializeApp(options);
+        	client = FirebaseMessaging.getInstance(app);
+    	} catch (IOException ex) {
+    		log.error("Error opening " + firebaseCredentialsPath, ex);
+		}
+    }
+
+    /**
+     * Finalizes the client. Prabably not needed, just to be sure.
+     */
+    @PreDestroy
+    void cleanup() {
+    	if (app != null) {
+        	app.delete();
+    	}
+    	app = null;
+    	client = null;
+    }
+
+    /**
+     * Checks the state of the client. When the client is missing an exception is thrown.
+     */
+    private void sanityCheck() {
+    	if (client == null) {
+    		throw new IllegalStateException("Firebase has not properly been initialized, cannot send messages.");
+    	}
+    }
+    
+    /**
+     * Sends a single message to a recipient.
+     * @param firebaseToken the firebase token of the recipient.
+     * @param msg The message to send.
+     */
+    public void send(String firebaseToken, NetMobielMessage msg) {
+    	send(firebaseToken, msg, false);
+    }
+
+    /**
+     * Sends a single message to a recipient.
+     * @param firebaseToken the firebase token of the recipient.
+     * @param msg The message to send.
+     * @param dryRun a boolean indicating whether to perform a dry run (validation only) of the send.
+     */
+    public void send(String firebaseToken, NetMobielMessage msg, boolean dryRun) {
+    	sanityCheck();
+    	// This registration token comes from the client FCM SDKs.
+	    // See documentation on defining a message payload.
+	    Notification notification = Notification.builder()
+	    		.setTitle(msg.getSubject())
+	    		.setBody(msg.getBody())
+	    		.build();
+	    Message message = Message.builder()
+		        .setToken(firebaseToken)
+			    .setNotification(notification)
+		        .putData("context", msg.getContext())
+		        .putData("creationTime", DateTimeFormatter.ISO_INSTANT.format(msg.getCreationTime()))
+		        .putData("senderId", msg.getSender().getManagedIdentity())
+		        .putData("senderGivenName", msg.getSender().getGivenName())
+		        .putData("senderFamilyName", msg.getSender().getFamilyName())
+		        .build();
+		try {
+		    // Send a message to the device corresponding to the provided registration token.
+			String response = client.send(message, dryRun);
+		    // Response is a message ID string.
+			if (log.isDebugEnabled()) {
+				log.debug("Message sent: " + response);
+			}
+		} catch (FirebaseMessagingException e) {
+			throw new SystemException("Failed to send message", e);
+		}
+    }
+
+    /**
+     * Sends a single message to multiple recipients (at most 500).
+     * @param firebaseTokens the firebase tokens of the recipients.
+     * @param msg The message to send.
+     */
+    public void send(Collection<String> firebaseTokens, NetMobielMessage msg) {
+    	send(firebaseTokens, msg, false);
+    }
+
+    /**
+     * Sends a single message to multiple recipients (at most 500).
+     * @param firebaseTokens the firebase tokens of the recipients.
+     * @param msg The message to send.
+     * @param dryRun a boolean indicating whether to perform a dry run (validation only) of the send.
+     */
+    public void send(Collection<String> firebaseTokens, NetMobielMessage msg, boolean dryRun) {
+    	sanityCheck();
+	    Notification notification = Notification.builder()
+	    		.setTitle(msg.getSubject())
+	    		.setBody(msg.getBody())
+	    		.build();
+    	MulticastMessage message = MulticastMessage.builder()
+        	    .addAllTokens(firebaseTokens)
+    		    .setNotification(notification)
+    	        .putData("context", msg.getContext())
+    	        .putData("creationTime", DateTimeFormatter.ISO_INSTANT.format(msg.getCreationTime()))
+    	        .putData("senderId", msg.getSender().getManagedIdentity())
+    	        .putData("senderGivenName", msg.getSender().getGivenName())
+    	        .putData("senderFamilyName", msg.getSender().getFamilyName())
+    	    .build();
+		try {
+	    	BatchResponse response = client.sendMulticast(message, dryRun);
+	    	// See the BatchResponse reference documentation for the contents of response.
+			if (log.isDebugEnabled()) {
+				log.debug(String.format("Messages sent: #%d, failed #%d", response.getSuccessCount(), response.getFailureCount()));
+			}
+		} catch (FirebaseMessagingException e) {
+			throw new SystemException("Failed to send batch message", e);
+		}
+    }
+
+    /**
+     * Publishes a single message to a topic.
+     * @param fcmTopic the name of the FCM topic to which the message should be sent.
+     * @param msg The message to send.
+     */
+    public void publish(String fcmTopic, NetMobielMessage msg) {
+    	publish(fcmTopic, msg, false);
+    }
+
+    /**
+     * Sends a single message to a topic.
+     * @param fcmTopic the name of the FCM topic to which the message should be sent.
+     * @param msg The message to send.
+     * @param dryRun a boolean indicating whether to perform a dry run (validation only) of the send.
+     */
+    public void publish(String fcmTopic, NetMobielMessage msg, boolean dryRun) {
+    	sanityCheck();
+    	// The topic name can be optionally prefixed with "/topics/".
+	    Notification notification = Notification.builder()
+	    		.setTitle(msg.getSubject())
+	    		.setBody(msg.getBody())
+	    		.build();
+	    Message message = Message.builder()
+		        .setTopic(fcmTopic)
+			    .setNotification(notification)
+		        .putData("context", msg.getContext())
+		        .putData("creationTime", DateTimeFormatter.ISO_INSTANT.format(msg.getCreationTime()))
+		        .putData("senderId", msg.getSender().getManagedIdentity())
+		        .putData("senderGivenName", msg.getSender().getGivenName())
+		        .putData("senderFamilyName", msg.getSender().getFamilyName())
+		        .build();
+		try {
+	    	// Send a message to the devices subscribed to the provided topic.
+	    	String response = client.send(message, dryRun);
+	    	// Response is a message ID string.
+			if (log.isDebugEnabled()) {
+				log.debug(String.format("Message sent to topic %s: %s", fcmTopic, response));
+			}
+		} catch (FirebaseMessagingException e) {
+			throw new SystemException("Failed to send topic message", e);
+		}
+    }
+}
---
+++ netmobiel-firebase-client/src/test/java/eu/netmobiel/firebase/messaging/FirebaseMessagingClientIT.java
@@ -0,0 +1,132 @@
+package eu.netmobiel.firebase.messaging;
+
+
+import java.io.File;
+import java.time.Instant;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+
+import eu.netmobiel.commons.model.NetMobielMessage;
+import eu.netmobiel.commons.model.NetMobielUser;
+
+@RunWith(Arquillian.class)
+public class FirebaseMessagingClientIT {
+    @Deployment
+    public static Archive<?> createTestArchive() {
+    	File[] deps = Maven.configureResolver()
+				.loadPomFromFile("pom.xml")
+				.importCompileAndRuntimeDependencies() 
+				.resolve()
+				.withTransitivity()
+				.asFile();
+		Archive<?> archive = ShrinkWrap.create(WebArchive.class, "test.war")
+       		.addAsLibraries(deps)
+            .addClass(FirebaseMessagingClient.class)
+//            .addClass(NetMobielUser.class)
+//            .addClass(NetMobielMessage.class)
+            // Arquillian tests need the beans.xml to recognize it as a CDI application
+            .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
+            // Take car of removing the default json provider, because we use jackson everywhere (unfortunately).
+        	.addAsWebInfResource("jboss-deployment-structure.xml")
+        	.addAsResource("log4j.properties");
+//		System.out.println(archive.toString(Formatters.VERBOSE));
+        return archive;
+    }
+
+    @Inject
+    private FirebaseMessagingClient client;
+
+    @SuppressWarnings("unused")
+	@Inject
+    private Logger log;
+
+    private NetMobielUser aSender;
+    
+    @Before
+    public void prepareTest() throws Exception {
+    	aSender = new TestUser("some-user", "Otto", "Normalverbraucher");
+    }
+    
+
+    @Test
+    public void testPublishMessage() throws Exception {
+    	NetMobielMessage msg = new TestMessage("a body", "urn:nb:ts:Test:1234", "Test 1234", Instant.now(), aSender);
+    	client.publish("systemTopic", msg, true);
+    	
+    }
+
+    private static class TestUser implements NetMobielUser {
+        private String managedIdentity;
+    	private String givenName;
+    	private String familyName;
+    	
+    	public TestUser(String id, String givenName, String familyName) {
+    		this.managedIdentity = id;
+    		this.givenName = givenName;
+    		this.familyName = familyName;
+    	}
+
+		public String getManagedIdentity() {
+			return managedIdentity;
+		}
+
+		public String getGivenName() {
+			return givenName;
+		}
+
+		public String getFamilyName() {
+			return familyName;
+		}
+    	
+    }
+
+    private static class TestMessage implements NetMobielMessage {
+    	private String body;
+    	private String context;
+    	private String subject;
+    	private Instant creationTime;
+        private NetMobielUser sender;
+
+        public TestMessage(String body, String context, String subject, Instant creationTime, NetMobielUser sender) {
+        	this.body = body;
+        	this.context = context;
+        	this.subject = subject;
+        	this.creationTime = creationTime;
+        	this.sender = sender;
+        }
+
+		public String getBody() {
+			return body;
+		}
+
+		public String getContext() {
+			return context;
+		}
+
+		public String getSubject() {
+			return subject;
+		}
+
+		public Instant getCreationTime() {
+			return creationTime;
+		}
+
+		public NetMobielUser getSender() {
+			return sender;
+		}
+        
+    }
+}
