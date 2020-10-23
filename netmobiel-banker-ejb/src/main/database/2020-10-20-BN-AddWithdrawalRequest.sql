1ce9c570f112f6ec42c5aa1e994c40a210a27434
Jaap Reitsma <jaap.reitsma@gmail.com>
2020-10-23 17:14:16 +0200

    Added a transaction reference.

--- netmobiel-banker-ejb/src/main/database/2020-10-20-BN-AddWithdrawalRequest.sql
+++ netmobiel-banker-ejb/src/main/database/2020-10-20-BN-AddWithdrawalRequest.sql
@@ -2,16 +2,17 @@
 CREATE TABLE public.payment_batch
 (
     id bigint NOT NULL,
-    completion_time timestamp without time zone,
     creation_time timestamp without time zone NOT NULL,
-    cleared_by bigint,
-    requested_by bigint NOT NULL,
+    created_by bigint NOT NULL,
+    settlement_time timestamp without time zone,
+    settled_by bigint,
+    order_reference character varying(32) COLLATE pg_catalog."default",
     CONSTRAINT payment_batch_pkey PRIMARY KEY (id),
-    CONSTRAINT payment_batch_cleared_by_fk FOREIGN KEY (cleared_by)
+    CONSTRAINT payment_batch_settled_by_fk FOREIGN KEY (settled_by)
         REFERENCES public.bn_user (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT payment_batch_requested_by_fk FOREIGN KEY (requested_by)
+    CONSTRAINT payment_batch_created_by_fk FOREIGN KEY (created_by)
         REFERENCES public.bn_user (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -37,21 +38,22 @@ CREATE TABLE public.withdrawal_request
     id bigint NOT NULL,
     amount_credits integer NOT NULL,
     amount_eurocents integer NOT NULL,
-    completed_time timestamp without time zone,
     creation_time timestamp without time zone NOT NULL,
+    created_by bigint NOT NULL,
+    settlement_time timestamp without time zone,
+    settled_by bigint,
     description character varying(128) COLLATE pg_catalog."default" NOT NULL,
     order_reference character varying(32) COLLATE pg_catalog."default",
     status character varying(1) COLLATE pg_catalog."default" NOT NULL,
     account bigint NOT NULL,
-    cleared_by bigint,
     payment_batch bigint,
-    requested_by bigint NOT NULL,
+    transaction bigint NOT NULL,
     CONSTRAINT withdrawal_request_pkey PRIMARY KEY (id),
     CONSTRAINT withdrawal_account_fk FOREIGN KEY (account)
         REFERENCES public.account (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT withdrawal_cleared_by_fk FOREIGN KEY (cleared_by)
+    CONSTRAINT withdrawal_settled_by_fk FOREIGN KEY (settled_by)
         REFERENCES public.bn_user (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
@@ -59,8 +61,12 @@ CREATE TABLE public.withdrawal_request
         REFERENCES public.payment_batch (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT withdrawal_requested_by_fk FOREIGN KEY (requested_by)
+    CONSTRAINT withdrawal_created_by_fk FOREIGN KEY (created_by)
         REFERENCES public.bn_user (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+    CONSTRAINT withdrawal_transaction_fk FOREIGN KEY (transaction)
+        REFERENCES public.accounting_transaction (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
---
+++ netmobiel-banker-ejb/src/main/database/2020-10-22-BN-AddDepositTransaction.sql
@@ -0,0 +1,9 @@
+-- Banker: Add a transaction reference to a deposit request
+ALTER TABLE public.deposit_request
+    ADD COLUMN transaction bigint,
+    ADD CONSTRAINT deposit_transaction_fk FOREIGN KEY (transaction)
+        REFERENCES public.accounting_transaction (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+;
