2f0d0fb16e97dd89e81e8e41692eabae88353f33
Jaap Reitsma <jaap.reitsma@gmail.com>
2021-01-13 09:57:07 +0100

    Set the version to a default value.

--- netmobiel-rideshare-ejb/src/main/database/2021-01-11-RS-AddRideVersion.sql
+++ netmobiel-rideshare-ejb/src/main/database/2021-01-11-RS-AddRideVersion.sql
@@ -1,4 +1,8 @@
 -- Rideshare: Add version column for optimistic locking 
 ALTER TABLE public.ride 
-	ADD COLUMN version integer
+	ADD COLUMN version integer default 0
 ;
+update public.ride set version = 0 where version is null;
+
+ALTER TABLE public.ride
+    ALTER COLUMN version SET NOT NULL;
