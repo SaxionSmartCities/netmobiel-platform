2b107653e74d52d4af7fa90225c912af99450a65
Jaap Reitsma <j.reitsma@saxion.nl>
2021-06-29 20:47:33 +0200

    NBL-571 Code cleanup.

--- netmobiel-commons/src/main/java/eu/netmobiel/commons/security/KeycloakSecurityFilter.java
+++ netmobiel-commons/src/main/java/eu/netmobiel/commons/security/KeycloakSecurityFilter.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javax.inject.Inject;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -23,10 +22,6 @@ public class KeycloakSecurityFilter implements Filter {
 
     @Inject
     private Logger log;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
