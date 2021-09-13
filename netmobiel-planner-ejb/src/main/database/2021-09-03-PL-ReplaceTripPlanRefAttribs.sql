8d264e2239dccb211293fc28c0e8d510679bdc99
Jaap Reitsma <j.reitsma@saxion.nl>
2021-09-13 10:35:54 +0200

    Fixed typo.

--- netmobiel-planner-ejb/src/main/database/2021-09-03-PL-ReplaceTripPlanRefAttribs.sql
+++ netmobiel-planner-ejb/src/main/database/2021-09-03-PL-ReplaceTripPlanRefAttribs.sql
@@ -6,8 +6,8 @@ ALTER TABLE public.trip_plan
     DROP COLUMN reference_fare_credits,
     ADD COLUMN geodesic_distance integer,
     ADD COLUMN reference_itinerary bigint,
-    ADD CONSTRAINT trip_plan_reference_itinerary_itinerary_uc UNIQUE (reference_itinerary_itinerary),
-	ADD CONSTRAINT trip_plan_reference_itinerary_itinerary_fk FOREIGN KEY (reference_itinerary_itinerary)
+    ADD CONSTRAINT trip_plan_reference_itinerary_itinerary_uc UNIQUE (reference_itinerary),
+	ADD CONSTRAINT trip_plan_reference_itinerary_itinerary_fk FOREIGN KEY (reference_itinerary)
         REFERENCES public.itinerary (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
