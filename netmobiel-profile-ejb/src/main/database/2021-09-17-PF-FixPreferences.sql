9b59834614abbb66e2a29666bf5fd7338cdde5df
Jaap Reitsma <j.reitsma@saxion.nl>
2021-09-21 18:07:26 +0200

    Repaired faulty statements.

--- netmobiel-profile-ejb/src/main/database/2021-09-17-PF-FixPreferences.sql
+++ netmobiel-profile-ejb/src/main/database/2021-09-17-PF-FixPreferences.sql
@@ -9,22 +9,22 @@ DELETE FROM public.passenger_luggage WHERE luggage = 'WC' OR luggage = 'WL'
 ALTER TABLE public.search_preferences
 	DROP COLUMN allow_transfers,
 	ADD COLUMN max_transfers integer,
-	ADD COLUMN needs_assistance NOT NULL boolean DEFAULT False
+	ADD COLUMN needs_assistance boolean NOT NULL DEFAULT False
 ;
 ALTER TABLE public.search_preferences
 	RENAME COLUMN max_transfer_time TO max_walk_distance
 ;
 
-UPDATE public.search_preferences SET max_walk_distance = 2000 WHERE max_walk_distance < 250;
+UPDATE public.search_preferences SET max_walk_distance = 250 WHERE max_walk_distance < 250;
 UPDATE public.search_preferences SET max_walk_distance = 500 WHERE max_walk_distance < 500;
 
 
 -- Remove Wheelchair (WC) and Walker (WL) from the rideshare luggage options.
 DELETE FROM public.rideshare_luggage WHERE luggage = 'WC' OR luggage = 'WL'
 
+ALTER TABLE public.rideshare_preferences RENAME COLUMN max_minutes_detour TO max_distance_detour;
 ALTER TABLE public.rideshare_preferences
-	RENAME COLUMN max_minutes_detour TO max_distance_detour
-	ADD COLUMN max_time_detour integer
+	ADD COLUMN max_time_detour integer,
 	ADD COLUMN able_to_assist boolean NOT NULL DEFAULT True
 ;
 UPDATE public.rideshare_preferences SET max_distance_detour = max_distance_detour * 1000;
