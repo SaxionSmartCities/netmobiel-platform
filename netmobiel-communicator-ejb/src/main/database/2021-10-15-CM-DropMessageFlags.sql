af58d7c83906fe85526ad907e8a8c2f109e75c20
Jaap Reitsma <j.reitsma@saxion.nl>
2021-10-29 22:14:03 +0200

    Renamed sql files to recognize module.

--- netmobiel-communicator-ejb/src/main/database/2021-10-15-CM-DropMessageFlags.sql
+++ netmobiel-communicator-ejb/src/main/database/2021-10-15-CM-DropMessageFlags.sql
@@ -1,6 +1,6 @@
 -- Delete the message to all
 
-DELETE FROM envelope e, message m WHERE e.message = m.id AND m.to_all_users = true;
+DELETE FROM envelope USING message WHERE envelope.message = message.id AND message.to_all_users = true;
 DELETE FROM message m WHERE m.to_all_users = true;
 
 ALTER TABLE public.message
