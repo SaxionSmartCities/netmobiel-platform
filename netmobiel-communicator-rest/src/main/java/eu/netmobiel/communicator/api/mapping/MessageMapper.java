bf45a82e20deca0459c13dedfcad5a01cf55d06c
Jaap Reitsma <j.reitsma@saxion.nl>
2021-11-18 22:03:31 +0100

    Attach envelopes also to message.

--- netmobiel-communicator-rest/src/main/java/eu/netmobiel/communicator/api/mapping/MessageMapper.java
+++ netmobiel-communicator-rest/src/main/java/eu/netmobiel/communicator/api/mapping/MessageMapper.java
@@ -1,7 +1,9 @@
 package eu.netmobiel.communicator.api.mapping;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 import eu.netmobiel.commons.model.PagedResult;
@@ -26,4 +28,9 @@ public abstract class MessageMapper {
 	@Mapping(target = "oldSender", ignore = true)
 	public abstract Message map(eu.netmobiel.communicator.api.model.Message source);
 
+	@AfterMapping
+    protected void AttachEnvelopesToMessage(eu.netmobiel.communicator.api.model.Message source, @MappingTarget Message target) {
+		// Assure the envelopes have an association with the Message.
+		target.getEnvelopes().forEach(env -> env.setMessage(target));
+	}
 }
