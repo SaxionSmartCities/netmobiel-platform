50c9e2551838edb8dd880cca21e4b544cabe85d8
Jaap Reitsma <j.reitsma@saxion.nl>
2022-04-09 16:48:57 +0200

    Added home_state_code too.

--- netmobiel-profile-ejb/src/main/database/2022-04-09-PF-ClearInvalidHome.sql
+++ netmobiel-profile-ejb/src/main/database/2022-04-09-PF-ClearInvalidHome.sql
@@ -3,7 +3,7 @@
 UPDATE public.profile SET home_country_code = 'NLD' WHERE home_country_code = 'NL';
 
 UPDATE public.profile SET home_point = null, home_locality = null, home_street = null, 
-	home_postal_code = null, home_house_nr = null, home_label = null
+	home_postal_code = null, home_house_nr = null, home_label = null, home_state_code = null
 	WHERE st_astext(home_point) = 'POINT(0 0)'
 ;
 
