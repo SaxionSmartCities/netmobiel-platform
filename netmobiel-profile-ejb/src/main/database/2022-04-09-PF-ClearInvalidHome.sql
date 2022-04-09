0048452cd9662b35fcc04279aaee0b12aaf5bf33
Jaap Reitsma <j.reitsma@saxion.nl>
2022-04-09 16:30:53 +0200

    Clear invalid home addresses.

---
+++ netmobiel-profile-ejb/src/main/database/2022-04-09-PF-ClearInvalidHome.sql
@@ -0,0 +1,10 @@
+-- Profile Service: Clear invalid home addresses
+
+UPDATE public.profile SET home_country_code = 'NLD' WHERE home_country_code = 'NL';
+
+UPDATE public.profile SET home_point = null, home_locality = null, home_street = null, 
+	home_postal_code = null, home_house_nr = null, home_label = null
+	WHERE st_astext(home_point) = 'POINT(0 0)'
+;
+
+
