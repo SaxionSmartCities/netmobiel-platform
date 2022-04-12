8571b9ff4262d3709940ed7d30c5c18288e0b4bb
Jaap Reitsma <j.reitsma@saxion.nl>
2022-04-12 15:13:45 +0200

    Implmented the /version endpoint in the profile service.

--- netmobiel-profile-rest/src/main/java/eu/netmobiel/profile/api/ProfileServiceApplication.java
+++ netmobiel-profile-rest/src/main/java/eu/netmobiel/profile/api/ProfileServiceApplication.java
@@ -31,7 +31,6 @@ import javax.ws.rs.core.Application;
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import org.slf4j.Logger;
 
-import eu.netmobiel.commons.Version;
 import eu.netmobiel.commons.jaxrs.BusinessExceptionMapper;
 import eu.netmobiel.commons.jaxrs.EJBExceptionMapper;
 import eu.netmobiel.commons.jaxrs.Jackson2ObjectMapperContextResolver;
@@ -46,6 +45,7 @@ import eu.netmobiel.profile.api.resource.ProfilesResource;
 import eu.netmobiel.profile.api.resource.ReviewsResource;
 import eu.netmobiel.profile.api.resource.SurveyInteractionsResource;
 import eu.netmobiel.profile.api.resource.TestsResource;
+import eu.netmobiel.profile.api.resource.VersionResource;
 
 
 /**
@@ -63,7 +63,7 @@ public class ProfileServiceApplication extends Application {
     private Logger log;
 
     @Inject
-    private Version version;
+    private ProfileServiceVersion version;
 
     private Set<Class<?>> resources = new HashSet<>();
     private Set<Object> singletons = new HashSet<>();
@@ -103,6 +103,7 @@ public class ProfileServiceApplication extends Application {
         resources.add(ReviewsResource.class);
         resources.add(SurveyInteractionsResource.class);
         resources.add(TestsResource.class);
+        resources.add(VersionResource.class);
         resources.add(OffsetDateTimeParamConverterProvider.class);
         resources.add(WebApplicationExceptionMapper.class);
         resources.add(EJBExceptionMapper.class);
@@ -126,8 +127,8 @@ public class ProfileServiceApplication extends Application {
         builder.append("\n------------------------------------------------");
         builder.append("\nStarting up Netmobiel Profile Service REST Service");
         builder.append("\n\tVersion:  " + version.getVersionString());
-        builder.append("\n\tBuilt On: " + version.getVersionDate().toString());
-        builder.append("\n\tBuild:    " + version.getVersionInfo());
+        builder.append("\n\tBuilt On: " + version.getVersionDate());
+        builder.append("\n\tBuild:    " + version.getCommitId());
         builder.append("\n------------------------------------------------");
         log.info(builder.toString());
    }
---
+++ netmobiel-profile-rest/src/main/java/eu/netmobiel/profile/api/resource/VersionResource.java
@@ -0,0 +1,63 @@
+package eu.netmobiel.profile.api.resource;
+
+import java.util.Optional;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import eu.netmobiel.commons.model.NetMobielUser;
+import eu.netmobiel.commons.security.SecurityIdentity;
+import eu.netmobiel.profile.api.ProfileServiceVersion;
+import eu.netmobiel.profile.api.VersionApi;
+import eu.netmobiel.profile.api.model.UserRef;
+import eu.netmobiel.profile.service.ProfileManager;
+
+@RequestScoped
+public class VersionResource implements VersionApi {
+
+	@Inject
+	protected SecurityIdentity securityIdentity;
+
+	@Inject
+	private ProfileServiceVersion version;
+
+	@Inject
+	private ProfileManager profileManager;
+
+	@Override
+	public Response getVersion() {
+		Response rsp = null;
+		try {
+			eu.netmobiel.profile.api.model.Version v = new eu.netmobiel.profile.api.model.Version();
+			v.setBuildTime(version.getVersionDate());
+			v.setBuildVersion(version.getVersionString());
+			v.setCommitId(version.getCommitId());
+			NetMobielUser nmuser = securityIdentity.getRealUser();
+			if (nmuser != null) {
+				UserRef ur = new UserRef();
+				ur.setId(nmuser.getManagedIdentity());
+				ur.setFirstName(nmuser.getGivenName());
+				ur.setLastName(nmuser.getFamilyName());
+				v.setCaller(ur);
+				String effCallerMid = securityIdentity.getEffectivePrincipal().getName();
+				if (!effCallerMid.equals(nmuser.getManagedIdentity())) {
+					Optional<NetMobielUser> effUser = profileManager.getNetMobielUser(effCallerMid);
+					if (effUser.isPresent()) {
+						UserRef eur = new UserRef();
+						eur.setId(effUser.get().getManagedIdentity());
+						eur.setFirstName(effUser.get().getGivenName());
+						eur.setLastName(effUser.get().getFamilyName());
+						v.setEffectiveCaller(eur);
+					}
+				}
+			}
+			rsp = Response.ok(v).build();
+		} catch (Exception e) {
+			throw new WebApplicationException(e);
+		}
+		return rsp;
+	}
+
+}
