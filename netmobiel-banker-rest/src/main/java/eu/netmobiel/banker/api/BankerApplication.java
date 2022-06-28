2366aed69a7235beb94b74b784977c4a8004735c
Jaap Reitsma <j.reitsma@saxion.nl>
2022-06-28 22:05:45 +0200

    Typo in log message.

--- netmobiel-banker-rest/src/main/java/eu/netmobiel/banker/api/BankerApplication.java
+++ netmobiel-banker-rest/src/main/java/eu/netmobiel/banker/api/BankerApplication.java
@@ -66,7 +66,7 @@ public class BankerApplication extends Application {
     public void postConstruct() {
         StringBuilder builder = new StringBuilder();
         builder.append("\n------------------------------------------------");
-        builder.append("\nStarting up Netmobiel Credit REST Service");
+        builder.append("\nStarting up Netmobiel Banker REST Service");
         builder.append("\n\tVersion:  " + version.getVersionString());
         builder.append("\n\tBuilt On: " + version.getVersionDate());
         builder.append("\n\tBuild:    " + version.getCommitId());
