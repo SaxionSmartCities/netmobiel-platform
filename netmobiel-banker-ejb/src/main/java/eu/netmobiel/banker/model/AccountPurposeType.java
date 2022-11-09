05fec8268b38839c89974f44fe7ea77aa25bfcf3
Jaap Reitsma <j.reitsma@saxion.nl>
2022-11-09 13:14:53 +0100

    Added commments.

--- netmobiel-banker-ejb/src/main/java/eu/netmobiel/banker/model/AccountPurposeType.java
+++ netmobiel-banker-ejb/src/main/java/eu/netmobiel/banker/model/AccountPurposeType.java
@@ -1,14 +1,23 @@
 package eu.netmobiel.banker.model;
 
 /**
- * Type of account usage.
+ * Type of account. Used to distinguish between different types of accounts and allowed operations.
  *  
  * @author Jaap Reitsma
  *
  */
 public enum AccountPurposeType {
+	/**
+	 * A system account.
+	 */
 	SYSTEM("S"),
+	/**
+	 * A user account holding the free credits.
+	 */
 	CURRENT("C"),
+	/**
+	 * A user account for holding the premium credits.
+	 */
 	PREMIUM("P");
 	
 	private String code;
